9be484e2320dc26c00fc61cd4f3785b354f098ad
Jake Ashton <jakemaddenashton@gmail.com>
2025-07-19 22:02:27 -0700

    feat: add session store library and initial store

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ const path = require("node:path");
 const { Pool } = require("pg");
 const express = require("express");
 const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 
@@ -19,7 +20,17 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(
+  session({
+    store: new pgSession({
+      pool: pool,
+      tableName: "session",
+    }),
+    secret: process.env.FOO_COOKIE_SECRET,
+    resave: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
+  })
+);
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
